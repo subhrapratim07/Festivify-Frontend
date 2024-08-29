73c6a81a95e00ca3752b7f64342e5b3ca5b77238
Subhra Pratim Mondal <160265255+subhrapratim07@users.noreply.github.com>
2024-08-30 04:29:20 +0530

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -43,7 +43,7 @@ function Login() {
           <form onSubmit={handleSubmit(onSubmit)} method="dialog">
             {/* if there is a button in form, it will close the modal */}
             <Link
-              to="/"
+              to="/table"
               className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
               onClick={() => document.getElementById("my_modal_3").close()}
             >
