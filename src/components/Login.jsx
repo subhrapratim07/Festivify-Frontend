1d42eba772563073f640826871684609464aff81
Subhra Pratim Mondal <160265255+subhrapratim07@users.noreply.github.com>
2024-08-30 09:52:16 +0530

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -39,7 +39,7 @@ function Login() {
   return (
     <div >
       <dialog id="my_modal_3" className="modal">
-        <div className="modal-box">
+        <div className="modal-box text-black">
           <form onSubmit={handleSubmit(onSubmit)} method="dialog">
             {/* if there is a button in form, it will close the modal */}
             <Link
